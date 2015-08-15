3f7585d2bafee5b9381c2ab4838dab835b54e960
Rube <353371737@qq.com>
2015-08-15 23:41:03 +0800

    sh - make jar change

--- src/android/tools/jarBuildTool.sh
+++ src/android/tools/jarBuildTool.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 cd ../src/rayna-native-lib/
-./gradlew makeJar
+./gradlew clean makeJar
 cp ../../build/rayna-native.jar ../../assert/rayna-native-template/app/libs
 
 #make android classes to jar
