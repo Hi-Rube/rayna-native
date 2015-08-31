3b98747025ce76fee2f7224ed68e1130417cde0e
Rube <353371737@qq.com>
2015-08-31 23:33:05 +0800

    web - webFramework base

--- src/rayna/lib/rayna-framework/web/rayna-webFramework.js
+++ src/rayna/lib/rayna-framework/web/rayna-webFramework.js
@@ -10,7 +10,9 @@ Rayna = {
     'cache': {
         'pageName': null,
         'classMap': {},
-        'idTree': {}
+        'idMap': {},
+        'nodeTree': [],
+        'moduleMap': {}
     },
     'page': function (pageName) {
         Rayna.cache['pageName'] = pageName;                     //应用加载时自动设定
@@ -23,8 +25,12 @@ Rayna = {
         }
     },
     'run': function (pageName, func) {
-        if (pageName == Rayna.cache['pageName']) {
-            window.onload = func;
+        if (!Rayna.cache['pageName'] || pageName == Rayna.cache['pageName']) {
+            document.addEventListener('DOMContentLoaded', function(){
+
+                Rayna._parse();
+                func();
+            }, false);
         } else {
             console.error(Rayna.name + ':pageName error');
         }
@@ -35,7 +41,83 @@ Rayna = {
     },
     '$': function (selector) {
 
+    },
+    'define': function () {
+
+        var arg = arguments;
+
+        switch (arg.length) {
+            case 0:
+                console.error(Rayna.name + ':can\'t run the module \"define()\"');
+                break;
+            case 1:
+                try {
+                    arg[0]();
+                } catch (e) {
+                    console.error(Rayna.name + ':can\'t run the module \"define(' + arg[0] + ')\"');
+                    console.log(e);
+                }
+                break;
+            case 2:
+                if (typeof arg[0] == 'string') {
+
+                    var moduleName = arg[0];
+                    if (typeof arg[1] == 'function') {
+
+                        if (R.cache.moduleMap[moduleName]) {
+                            return console.error(Rayna.name + ':' + moduleName + 'already exist in scope');
+                        }
+                        R.cache.moduleMap[moduleName] = new Rayna._Module(arg[1]);
+                    } else {
+                        R.cache.moduleMap[moduleName] = new Rayna._Module(function () {
+                            return arg[1];
+                        })
+                    }
+                } else {
+                    console.error(Rayna.name + ':can\'t run the module \"define(' + arg[0] + ',' + arg[1] + ')\"');
+                }
+                break;
+        }
+    },
+    'require': function (moduleName) {
+        return R.cache.moduleMap[moduleName].func();
     }
+};
+
+Rayna._Module = function (func) {
+    this.func = func;
+};
+
+Rayna._Node = function (tagName, id, className) {
+    this.tagName = tagName;
+    this.id = id;
+    this.className = className;
+    this.component = tagName != 'script' ? R.require('component-' + tagName) : null;
+    this.children = [];
+};
+
+Rayna._Node.prototype.addChildren = function (child) {
+    this.children.push(child);
+};
+
+Rayna._parse = function () {
+
+    var body = document.getElementsByTagName('body');
+
+    function walk(dom) {
+
+        for (var i = 0; i < dom.children.length; i++) {
+
+            var child = dom.children[i];
+            var tagName = child.tagName.toLowerCase();
+            var id = child.id;
+            var className = child.className;
+            var nowNode = new Rayna._Node(tagName, id, className);
+            Rayna.cache.nodeTree.push(nowNode);
+        }
+    }
+
+    walk(body[0]);
 };
 
 R = Rayna;
