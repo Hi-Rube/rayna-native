b75403ea80f1a1f940f42440a09a7534a9105558
Rube <353371737@qq.com>
2015-09-02 15:55:00 +0800

    web - dom 遍历

--- src/rayna/lib/rayna-framework/web/rayna-webFramework.js
+++ src/rayna/lib/rayna-framework/web/rayna-webFramework.js
@@ -9,8 +9,6 @@ Rayna = {
     'name': 'Rayna 0.1',
     'cache': {
         'pageName': null,
-        'classMap': {},
-        'idMap': {},
         'nodeTree': [],
         'moduleMap': {}
     },
@@ -26,7 +24,7 @@ Rayna = {
     },
     'run': function (pageName, func) {
         if (!Rayna.cache['pageName'] || pageName == Rayna.cache['pageName']) {
-            document.addEventListener('DOMContentLoaded', function(){
+            document.addEventListener('DOMContentLoaded', function () {
 
                 Rayna._parse();
                 func();
@@ -41,6 +39,28 @@ Rayna = {
     },
     '$': function (selector) {
 
+        var selector = selector.trim();
+        var selectorArr = selector.split(' ');
+
+        function walk(domain, saIndex) {                     //暂时只支持简单地选择器
+
+            if (saIndex == selectorArr.length) {
+                return domain;
+            }
+
+            var saItem = selectorArr[saIndex];
+            switch (saItem.charAt(0)) {
+                case '#':
+                    var content = saItem.substr(1, saItem.length - 1);
+                    return walk(domain._childrenIdMap[content], saIndex + 1);
+                    break;
+                case '.':
+                    break;
+                default :
+            }
+        }
+
+        return walk(Rayna.cache.nodeTree[0], 0);
     },
     'define': function () {
 
@@ -80,6 +100,9 @@ Rayna = {
         }
     },
     'require': function (moduleName) {
+        if (!R.cache.moduleMap[moduleName]) {
+            return null;
+        }
         return R.cache.moduleMap[moduleName].func();
     }
 };
@@ -88,23 +111,56 @@ Rayna._Module = function (func) {
     this.func = func;
 };
 
-Rayna._Node = function (tagName, id, className) {
+Rayna._Node = function (tagName, id, className, dom) {
     this.tagName = tagName;
     this.id = id;
     this.className = className;
-    this.component = tagName != 'script' ? R.require('component-' + tagName) : null;
+    this.dom = dom;
+    this.component = (R.require('component-' + tagName) && tagName != 'script' && tagName != 'body') ?
+        R.require('component-' + tagName).build(dom) :
+        null;
     this.children = [];
+    this.parent = null;
+    this._childrenClassMap = {};
+    this._childrenIdMap = {};
 };
 
 Rayna._Node.prototype.addChildren = function (child) {
     this.children.push(child);
 };
 
+Rayna._Node.prototype.setParent = function (parent) {
+    this.parent = parent;
+};
+
 Rayna._parse = function () {
 
     var body = document.getElementsByTagName('body');
+    var nodeTree = Rayna.cache.nodeTree;
 
-    function walk(dom) {
+    function restoreClassAndId(className, id, node) {
+
+        if (!node.parent) {
+            return;
+        }
+
+        var classMap = node.parent._childrenClassMap;
+        var idMap = node.parent._childrenIdMap;
+
+        if (className) {
+            classMap[className] || (classMap[className] = []);
+            classMap[className].push(node);
+        }
+        if (id) {
+            if (idMap[id]) {
+                console.error(Rayna.name + ': id:\"' + id + '\" has already exist');
+            }
+            idMap[id] = node;
+        }
+        restoreClassAndId(className, id, node.parent);
+    }
+
+    function walk(dom, parent) {
 
         for (var i = 0; i < dom.children.length; i++) {
 
@@ -112,12 +168,20 @@ Rayna._parse = function () {
             var tagName = child.tagName.toLowerCase();
             var id = child.id;
             var className = child.className;
-            var nowNode = new Rayna._Node(tagName, id, className);
-            Rayna.cache.nodeTree.push(nowNode);
+            var nowNode = new Rayna._Node(tagName, id, className, child);
+
+            nodeTree.push(nowNode);
+
+            nowNode.setParent(parent);
+            parent.addChildren(child);
+            restoreClassAndId(className, id, nowNode);
+            walk(child, nowNode);
         }
     }
 
-    walk(body[0]);
+    var rootNode = new Rayna._Node('body');
+    nodeTree.push(rootNode);
+    walk(body[0], rootNode);
 };
 
 R = Rayna;
